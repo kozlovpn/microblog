d3e140ea85e13f92adfee435cd7fd3d0f2f7c94a
Mikhail Ivanov <mivanov@mirantis.com>
2015-05-07 18:18:47 +0300

    Small typo fixes

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired, Email
 
 
 class LoginForm(Form):
-    email = StringField('email', validators = [Email(), DataRequired()])
-    password = PasswordField('password', validators = [DataRequired()])
-    remember_me = BooleanField('remember_me', default = False)
+    email = StringField('email', validators=[Email(), DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
